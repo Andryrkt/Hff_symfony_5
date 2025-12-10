9aca8a19eb40dc5c2522521baa5308ff89140e7c
Andryrkt <andryrkt08@gmail.com>
2025-12-10 10:24:21 +0300

    test(organisation): reorganistion des dossier de test

---
+++ tests/Unit/Entity/UserRoleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Tests\Unit\Entity;
+
+use App\Entity\Admin\PersonnelUser\User;
+use PHPUnit\Framework\TestCase;
+
+class UserRoleTest extends TestCase
+{
+    public function testRoles()
+    {
+        $user = new User();
+        $user->setRoles(['ROLE_ADMIN', 'ROLE_USER']);
+        $roles = $user->getRoles();
+
+        $this->assertContains('ROLE_ADMIN', $roles);
+        $this->assertContains('ROLE_USER', $roles);
+        $this->assertContains('ROLE_USER', $roles); // Symfony ajoute toujours ROLE_USER
+    }
+}
