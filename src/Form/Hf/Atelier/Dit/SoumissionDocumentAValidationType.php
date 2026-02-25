c2d6c8df305083178040b26e8777f1fcb933d1fe
Andryrkt <andryrkt08@gmail.com>
2026-02-25 14:09:13 +0300

    creation du formulaire SoumissionDocumenetAValidation

---
+++ src/Form/Hf/Atelier/Dit/SoumissionDocumentAValidationType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form\Hf\Atelier\Dit;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class SoumissionDocumentAValidationType extends AbstractType
+{
+    const DOC_A_SOUMETTRE = [
+        'OR' => 'OR',
+        'RI' => 'RI',
+        'FACTURE' => 'FACTURE',
+        'DEVIS - Vérification de prix' => 'DEVIS-VP',
+        'DEVIS - Validation atelier' => 'DEVIS-VA',
+        'BC' => 'BC'
+    ];
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add(
+                'SoumissionDocumentAValidation',
+                ChoiceType::class,
+                [
+                    'label' => 'Docs à intégrer dans DW',
+                    'choices' => self::DOC_A_SOUMETTRE,
+                    'placeholder' => '--',
+                ]
+            );
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => null,
+        ]);
+    }
+}
